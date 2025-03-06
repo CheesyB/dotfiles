eb51de42e4a815b23dff5a66cba88a53bdbbec76
Tim Breu <tim.breu@maibornwolff.de>
2025-03-06 10:43:07 +0100

    initial commit with homemanager

---
+++ .zshenv
@@ -0,0 +1,3 @@
+. "$HOME/.cargo/env"
+export PATH=~/bin:$PATH
+
---
+++ aliases.zsh
@@ -0,0 +1,11 @@
+alias v="nvim"
+alias myz="v ~/.zshrc" 
+alias zsource="source ~/.zshrc"
+
+alias gis="git status"
+alias ghard="git reset --hard"
+
+#alias cd="z"
+
+
+
